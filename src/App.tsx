8966303bc0a30bbfd429ef14f101332903838d8c
mateuszpolis <polismateusz@gmail.com>
2023-07-04 21:40:13 +0200

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,16 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1 className="text-3xl font-bold underline text-red-600">
+                Simple React Typescript Tailwind Sample
+              </h1>
+            </div>
+          }
+        />
         <Route path="/about" element={<div>About</div>} />
       </Routes>
     </div>
